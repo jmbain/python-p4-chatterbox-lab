a27cfe4a913c04db838a4cfee5d4769a7268fe86
Josh Bain <joshuambain@gmail.com>
2024-09-17 16:06:24 -0400

    completed chatterbox lab, flask application that handles crud operations

--- server/app.py
+++ server/app.py
@@ -14,13 +14,47 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        all_messages = Message.query.all()
+        return [message.to_dict() for message in all_messages], 200
+    
+    if request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data.get('body'),
+            username=data.get('username')
+        )
+        
+        db.session.add(new_message)
+        db.session.commit()
 
-@app.route('/messages/<int:id>')
+        return new_message.to_dict(), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    #Query and filter for Message objects whose id attribute matches the id parameter from the route
+    msg = Message.query.filter(Message.id == id).first()
+    if request.method == 'PATCH': 
+        #return error message and 404 if msg not found
+        if msg is None:
+            return {"error": "Message not found..."}, 404
+        # get json from request object
+        data = request.get_json()
+        #update the body of msg with the body in data
+        if 'body' in data:
+            msg.body = data['body']
+        #add and commit to db
+        db.session.add(msg)
+        db.session.commit()
+        #return SERIALIZED msg after updated
+        return msg.to_dict(), 200
+    
+    if request.method == "DELETE":
+        db.session.delete(msg)
+        db.session.commit()
+        return {}, 200
 
 if __name__ == '__main__':
     app.run(port=5555)
